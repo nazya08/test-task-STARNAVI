7512bdbb6f4e5eee7d0c6b0d71d9d45eebe79313
Nazar <nazarfilonyk@gmail.com>
2024-08-07 20:20:27 +0300

    Upgraded main.py and config.py; Created adapters/sqlalchemy/db directory

---
+++ src/main/config.py
@@ -0,0 +1,61 @@
+import os
+from typing import Optional, Any, Dict, List, Union
+
+from dotenv import load_dotenv
+from pydantic import PostgresDsn, field_validator, AnyHttpUrl
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Test task for StarNavi"
+    API_STR: str = "/api"
+
+    SERVER_NAME: str = "tz_starnavi"
+    SERVER_HOST: str = "0.0.0.0"
+    SERVER_PORT: int = 8000
+    LOG_LEVEL: str = "debug"
+    RELOAD: bool = True
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @field_validator("BACKEND_CORS_ORIGINS", mode="before")
+    def assemble_cors_origins(
+            cls, v: Union[str, List[str]]
+    ) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    # POSTGRES_SERVER: str = 'localhost:5432'
+    POSTGRES_SERVER: str = (
+        f'{os.getenv("POSTGRES_HOST")}:{os.getenv("POSTGRES_PORT")}'
+    )
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
+    SQLALCHEMY_DATABASE_URL: Optional[PostgresDsn] = None
+
+    @field_validator("SQLALCHEMY_DATABASE_URL", mode="before")
+    def assemble_db_connection(
+            cls, v: Optional[str], values: Dict[str, Any]
+    ) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
